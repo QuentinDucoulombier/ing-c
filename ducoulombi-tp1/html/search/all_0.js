fd59012e546ed8627859af6ac3e7f777276c9163
ducoulombi <ducoulombi@cy-tech.fr>
2022-10-29 00:36:41 +0200

    clear code

--- ducoulombi-tp1/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['helloworld_2ec_0',['helloWorld.c',['../helloWorld_8c.html',1,'']]]
-];
--- ducoulombi-tp1/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['td1_20_3a_20le_20hello_20wolrd_20francais_20_3achicken_3a_2',['TD1 : Le Hello wolrd Francais :chicken:',['../md_README.html',1,'']]]
-];

13d46fed3d7dd2b9693ebaa84ddf6de1d6794273
ducoulombi <ducoulombi@cy-tech.fr>
2022-11-16 19:16:19 +0100

    $l

--- ducoulombi-tp4/racine2.h
+++ ducoulombi-tp4/racine2.h
@@ -23,10 +23,30 @@
  */
 float racineNewton(int nbrIteration);
 
-
+/**
+ * @fn float racineSmyrne(int nbrIteration);
+ * @author Quentin Ducoulombier (ducoulombi@cy-tech.fr)
+ * @version 0.1
+ * @date 2022-11-16
+ * 
+ * @brief 
+ * 
+ * @param nbrIteration 
+ * @return float 
+ */
 float racineHalley(int nbrIteration);
 
-
+/**
+ * @fn float racineSmyrne(int nbrIteration);
+ * @author Quentin Ducoulombier (ducoulombi@cy-tech.fr)
+ * @version 0.1
+ * @date 2022-11-16
+ * 
+ * @brief 
+ * 
+ * @param nbrIteration 
+ * @return float 
+ */
 float racineSmyrne(int nbrIteration);
 
 #endif

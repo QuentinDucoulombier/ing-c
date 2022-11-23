fdd44279b6bf2cd2183d24e5880e746a35fb1627
ducoulombi <ducoulombi@cy-tech.fr>
2022-11-23 13:02:14 +0100

    Ajout tp Make + autre

---
+++ ducoulombi-tp5/save/main.c
@@ -0,0 +1,28 @@
+/**
+ * @file main.c
+ * @author Quentin Ducoulombier (ducoulombi@cy-tech.fr)
+ * @version 0.1
+ * @date 2022-11-23
+ * 
+ * @brief 
+ * 
+ */
+#include <stdio.h> 
+
+/**
+ * @fn int main(int argc, char *argv[])
+ * @author Quentin Ducoulombier (ducoulombi@cy-tech.fr)
+ * @version 0.1
+ * @date 2022-11-23
+ * 
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char *argv[])
+{
+    printf("Hello World !");
+    return (0);
+}
